0a2839ef5b041cf50caa7145a5101e8bb7d6bdf1
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-05-08 10:54:21 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    class Program {
+        static void Main(string[] args) {
+            //インスタンス生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(1235, "大福もち", 160);
+            Console.WriteLine("かりんとう税込価格:" + karinto.GetPriceIncloudingTax());
+            Console.WriteLine("大福もち税込価格:" + daifuku.GetPriceIncloudingTax());
+        }
+    }
+}
