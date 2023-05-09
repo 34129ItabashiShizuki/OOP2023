18e94d19d6534394b5c1fe49a8e31cf024f8c8ca
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-05-09 09:58:16 +0900

    0509演習2

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -16,19 +16,43 @@ namespace ProductSample {
             //Console.WriteLine("大福もち税込価格:" + daifuku.GetPriceIncloudingTax());
             #endregion
 
-            DateTime date = new DateTime(2023, 5, 8);
-            //DateTime date = DateTime.Today;
+            #region 0508演習1
+            //DateTime date = new DateTime(2023, 5, 8);
+            ////DateTime date = DateTime.Today;
 
-            //今日の日付
-            Console.WriteLine(date.Year + "年" + date.Month + "月" + date.Day + "日");
+            ////今日の日付
+            //Console.WriteLine(date.Year + "年" + date.Month + "月" + date.Day + "日");
 
-            //10日後を求める
-            DateTime daysAfter10 = date.AddDays(10);
-            Console.WriteLine("10日後は" + daysAfter10.Month + "月" + daysAfter10.Day + "日です。");
+            ////10日後を求める
+            //Console.WriteLine("10日後は" + date.Adddays(10).Year+ "年"+ date.Adddays(10).Month+ "月"+ date.Adddays(10).Day + "日です。");
 
-            //10日前を求める
-            DateTime daysbefore10 = date.AddDays(-10);
-            Console.WriteLine("10日前は" + daysbefore10.Month + "月" + daysbefore10.Day + "日です。");
+            ////10日前を求める
+            //Console.WriteLine("10日前は" + date.Adddays(10).Year + "年"+ date.Adddays(-10).Month + "月" + date.Adddays(-10).Day + "日です。");
+            #endregion
+            #region 0509演習2
+            Console.WriteLine("誕生日を入力");
+
+            //西暦の入力
+            Console.Write("西暦:");
+            int Ad = int.Parse(Console.ReadLine());
+            
+            //月の入力
+            Console.Write("月:");
+            int Month = int.Parse(Console.ReadLine());
+
+            //日の入力
+            Console.Write("日:");
+            int Day = int.Parse(Console.ReadLine());
+
+            DateTime birth = new DateTime(Ad, Month, Day);
+            DateTime today = DateTime.Today;
+
+            TimeSpan ts = today - birth;
+
+            Console.WriteLine("あなたは生まれてから今日まで{0}日目です", ts.Days);
+            #endregion
+
+            //Console.Write("あなたは{0}曜日に生まれました",ts.Days);
         }
     }
 }
