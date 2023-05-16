eb386bad8cab31155edd89523fa581a65f9a6976
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-05-16 10:22:09 +0900

    ReadSalesメソッドをSalesCounterクラスへ移動

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -14,29 +14,6 @@ namespace SalesCalculator {
                 Console.WriteLine("{0}{1:N0}", obj.Key, obj.Value);
             }
         }
-        //売上データを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-            //売上データを格納する
-            List<Sale> sales = new List<Sale>();
 
-            //ファイルからすべてのデータを読み込む
-            String[] lines = File.ReadAllLines(filePath);
-
-            //すべての行から一行ずつ取り出す
-            foreach(string line in lines) {
-                //区切りで項目別に分ける
-                String[] items = line.Split(',');
-                //Saleインスタンス生成
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[0],
-                    Amount = int.Parse(items[2])
-                };
-                //Saleインスタンスをコレクションに追加
-                sales.Add(sale);
-            }
-            return sales;
-        }
-        
     }
 }
--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,29 @@ namespace SalesCalculator {
                     dict[sale.ShopName] = sale.Amount;  //店名が存在しない(新規格納)
             }
             return dict;
+        }
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath) {
+            //売上データを格納する
+            List<Sale> sales = new List<Sale>();
+
+            //ファイルからすべてのデータを読み込む
+            String[] lines = File.ReadAllLines(filePath);
+
+            //すべての行から一行ずつ取り出す
+            foreach (string line in lines) {
+                //区切りで項目別に分ける
+                String[] items = line.Split(',');
+                //Saleインスタンス生成
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[0],
+                    Amount = int.Parse(items[2])
+                };
+                //Saleインスタンスをコレクションに追加
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
 }
