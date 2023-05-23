9bd082483da55396edf35e83261f44e6ee12953e
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-05-23 10:30:42 +0900

    P87

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -10,9 +10,14 @@ namespace Section01 {
         static void Main(string[] args) {
 
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            int count = numbers.Count(n => n % 2 == 0);
 
-            Console.WriteLine(count);
+            //5の倍数をカウント
+            //int count = numbers.Count(n => 0 < n && n % 5 == 0);
+
+            //合計
+            var sum = numbers.Where(n => n % 2 == 0).Average();
+
+            Console.WriteLine(sum);
         }
     }
 }
