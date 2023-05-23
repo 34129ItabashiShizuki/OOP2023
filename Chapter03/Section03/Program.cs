6a6dc1868a529db53fd7daa8857649a112e4789c
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-05-23 11:54:12 +0900

    第3章の演習問題1

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -10,6 +10,21 @@ namespace Section03 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //var exists = list.Exists(s => s[0] == 'C');
+            //Console.WriteLine(exists);
+
+            //var name = list.Find(s=> s.Length == 6);
+            //Console.WriteLine(name);
+
+            //var index = list.FindIndex(s => s == "Berlin");
+            //Console.WriteLine(index);
+
+            //var names = list.FindAll(s => s.Length <= 5);
+            //names.ForEach(s=>Console.WriteLine(s));
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
         }
     }
 }
