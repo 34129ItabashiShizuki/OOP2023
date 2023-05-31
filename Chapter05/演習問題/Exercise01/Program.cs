508ec481725df411257e970408eaa5017139a8d5
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-05-31 12:07:46 +0900

    5.15.2

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("文字列1:");
+            var s1 = Console.ReadLine();
+
+            Console.Write("文字列2:");
+            var s2 = Console.ReadLine();
+
+            if (String.Compare(s1,s2,true) == 0){
+                Console.WriteLine("等しい");
+            }
+            else {
+                Console.WriteLine("等しくない");
+            }
+
+        }
+    }
+}
