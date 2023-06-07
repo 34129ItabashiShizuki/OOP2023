314f3c568c964a9a989c8c30fb358966c666f819
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-06-07 09:56:09 +0900

    タイトルの長い順に出力

--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -14,12 +14,20 @@ namespace Section01 {
             var books = Books.GetBooks();
 
             //500円以上の本のタイトルをすべて出力
-            //タイトルに「物語」という文字列が含まれている書籍の平均ページ数を出力
 
+            #region タイトルに「物語」という文字列が含まれている書籍の平均ページ数を出力
             var booksObj = books.Where(x => x.Title.Contains("物語"))
                                     .Average(x => x.Pages);
 
-           Console.WriteLine(booksObj);
+            Console.WriteLine(booksObj);
+            #endregion
+
+            #region タイトルの長い順に出力
+            var title = books.OrderByDescending(x => x.Title.Length);
+            foreach (var item in title) {
+                Console.WriteLine(item.Title);
+            }
+            #endregion
         }
     }
 }
