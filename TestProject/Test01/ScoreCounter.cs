7e1d705c2cb2ad7461f79de720a52c25db01c25f
板𣘺 靖稀 <OIBC34129@ojs.ac.jp>
2023-06-19 15:38:15 +0900

    設問1_完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,41 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
-        }
-
-
-        //メソッドの概要： 
-        private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            _score = ReadScores(filePath);
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
-
-
-
-
-            
+            var dict = new SortedDictionary<string, int>();
+            foreach (var scores in _score) {
+                if (dict.ContainsKey(scores.Subject))
+                    dict[scores.Subject] += scores.Score; //科目名が既に存在する(点数加算)
+                else
+                    dict[scores.Subject] = scores.Score;  //科目名が存在しない(新規格納)
+            }
+            return dict;
         }
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScores(string filePath) {
+
+            var scores = new List<Student>();
+
+            var lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(student);
+            }
+            return scores;
+
+        }
+
     }
 }
