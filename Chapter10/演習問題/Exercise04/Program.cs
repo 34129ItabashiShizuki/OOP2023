d9e8c6e1c7d92dcff900ade3f9da84bd500cc84b
板橋靖稀 <OIBC34129@ojs.ac.jp>
2023-10-05 11:25:39 +0900

    Chapter10 演習問題Exercise02.03

---
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+
+
+
+
+            //書き込み
+            //File.WriteAllLines
+
+            // これ以降は確認用
+            var text = File.ReadAllText("sample.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
