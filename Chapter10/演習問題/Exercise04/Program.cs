d29c4891d554a33f61bf74b7be4cac8d62cb7aa0
板橋靖稀 <OIBC34129@ojs.ac.jp>
2023-10-10 11:52:57 +0900

    演習問題10 10.3まで

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
 
+            var newline = lines.Select(s => Regex.Replace
+                                (s, @"\b(v|V)ersion\s*=\s*""v4.0""", @"version = ""v5.0"""));
+
 
 
 
             //書き込み
-            //File.WriteAllLines
+            File.WriteAllLines("sample.txt",newline);
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
